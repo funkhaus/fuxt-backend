51f5f0f8ec88541b6d84253ea9f00a72fe8f1e02
Drew Baker <drewrb@gmail.com>
2019-06-25 17:25:31 -0700

    Removed not used Theme Settings

--- functions/wp-functions.php
+++ functions/wp-functions.php
@@ -10,15 +10,6 @@
         // Enable excerpts for pages
         add_post_type_support('page', 'excerpt');
 
-		// Add theme options page
-	    // if( function_exists('acf_add_options_page') ) {
-	    // 	acf_add_options_page(array(
-	    // 		'page_title' => 'Theme Settings',
-	    // 		'menu_title'	=> 'Theme Settings',
-	    // 		'menu_slug' 	=> 'theme-settings'
-	    // 	));
-	    // }
-
     }
     add_action('init', 'custom_wordpress_setup');
 
