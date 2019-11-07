8a713f63643d696dc829ee122b5a3f9641eeb361
Drew Baker <drewrb@gmail.com>
2019-11-07 11:02:40 -0800

    Added filter to increase posts per query, cleaned up comments

--- functions/gql-functions.php
+++ functions/gql-functions.php
@@ -2,184 +2,198 @@
 /*
  * Expose Google Analytics and theme screenshot to WP-GQL API
  */
-function whitelist_settings()
-{
-    // Allow some WordPress settings to be exposed to WP-GQL
-    $args = array(
-        'sanitize_callback' => 'esc_attr',
-        'show_in_graphql' => true
-    );
-    register_setting('general', 'ga_tracking_code_1', $args);
-    register_setting('general', 'ga_tracking_code_2', $args);
+    function whitelist_settings() {
+        // Allow some WordPress settings to be exposed to WP-GQL
+        $args = array(
+            'sanitize_callback' => 'esc_attr',
+            'show_in_graphql' => true
+        );
+        register_setting('general', 'ga_tracking_code_1', $args);
+        register_setting('general', 'ga_tracking_code_2', $args);
 
-    // Define a custom field to get Theme screenshot URL
-    register_graphql_field('GeneralSettings', 'themeScreenshotUrl', [
-        'type' => 'String',
-        'description' => __('URL to the active theme screenshot', 'stackhaus'),
-        'resolve' => function ($root, $args, $context, $info) {
-            $theme = wp_get_theme();
-            $url = "";
-            if ($theme->screenshot) {
-                $url = get_template_directory_uri() . "/" . $theme->screenshot;
+        // Define a custom field to get Theme screenshot URL
+        register_graphql_field('GeneralSettings', 'themeScreenshotUrl', [
+            'type' => 'String',
+            'description' => __('URL to the active theme screenshot', 'stackhaus'),
+            'resolve' => function ($root, $args, $context, $info) {
+                $theme = wp_get_theme();
+                $url = "";
+                if ($theme->screenshot) {
+                    $url = get_template_directory_uri() . "/" . $theme->screenshot;
+                }
+                return $url;
             }
-            return $url;
-        }
-    ]);
-}
-add_action('graphql_init', 'whitelist_settings', 1);
+        ]);
+    }
+    add_action('graphql_init', 'whitelist_settings', 1);
 
-/**
- *
+
+/*
  * Adds next post node to all the custom Post Types
- *
  */
-function gql_register_next_post()
-{
-    $post_types = WPGraphQL::$allowed_post_types;
+    function gql_register_next_post() {
+        $post_types = WPGraphQL::$allowed_post_types;
 
-    if (!empty($post_types) && is_array($post_types)) {
-        foreach ($post_types as $post_type) {
-            $post_type_object = get_post_type_object($post_type);
+        if (!empty($post_types) && is_array($post_types)) {
+            foreach ($post_types as $post_type) {
+                $post_type_object = get_post_type_object($post_type);
 
-            // Get the Type name with ucfirst
-            $ucfirst = ucfirst($post_type_object->graphql_single_name);
+                // Get the Type name with ucfirst
+                $ucfirst = ucfirst($post_type_object->graphql_single_name);
 
-            // Register a new Edge Type
-            register_graphql_type('Next' . $ucfirst . 'Edge', [
-                'fields' => [
-                    'node' => [
-                        'description' => __(
-                            'The node of the next item',
-                            'wp-graphql'
-                        ),
-                        'type' => $ucfirst,
-                        'resolve' => function ($post_id, $args, $context) {
-                            return \WPGraphQL\Data\DataSource::resolve_post_object(
-                                $post_id,
-                                $context
-                            );
-                        }
+                // Register a new Edge Type
+                register_graphql_type('Next' . $ucfirst . 'Edge', [
+                    'fields' => [
+                        'node' => [
+                            'description' => __(
+                                'The node of the next item',
+                                'wp-graphql'
+                            ),
+                            'type' => $ucfirst,
+                            'resolve' => function ($post_id, $args, $context) {
+                                return \WPGraphQL\Data\DataSource::resolve_post_object(
+                                    $post_id,
+                                    $context
+                                );
+                            }
+                        ]
                     ]
-                ]
-            ]);
+                ]);
 
-            // Register the next{$type} field
-            register_graphql_field($ucfirst, 'next' . $ucfirst, [
-                'type' => 'Next' . $ucfirst . 'Edge',
-                'description' => __(
-                    'The next post of the current port',
-                    'wp-graphql'
-                ),
-                'resolve' => function ($post_object, $args, $context) {
-//                     global $post;
-                    $post = get_post($post_object->postId);
-					if (is_post_type_hierarchical($post->post_type)) {
-	                    $post_id = get_next_page_id($post);
+                // Register the next{$type} field
+                register_graphql_field($ucfirst, 'next' . $ucfirst, [
+                    'type' => 'Next' . $ucfirst . 'Edge',
+                    'description' => __(
+                        'The next post of the current port',
+                        'wp-graphql'
+                    ),
+                    'resolve' => function ($post_object, $args, $context) {
+                        // global $post;
+                        $post = get_post($post_object->postId);
+    					if (is_post_type_hierarchical($post->post_type)) {
+    	                    $post_id = get_next_page_id($post);
+                        }
+                        else {
+    	                    $next_post = get_next_post();
+    	                    $post_id = $next_post->ID;
+                        }
+                        return $post_id;
                     }
-                    else {
-	                    $next_post = get_next_post();
-	                    $post_id = $next_post->ID;
-                    }
-                    return $post_id;
-                }
-            ]);
+                ]);
+            }
         }
     }
-}
-add_action('graphql_register_types', 'gql_register_next_post');
+    add_action('graphql_register_types', 'gql_register_next_post');
 
-/**
- *
+
+/*
  * Adds previous post node to all the custom Post Types
- *
  */
-function gql_register_previous_post()
-{
-    $post_types = WPGraphQL::$allowed_post_types;
+    function gql_register_previous_post() {
+        $post_types = WPGraphQL::$allowed_post_types;
 
-    if (!empty($post_types) && is_array($post_types)) {
-        foreach ($post_types as $post_type) {
-            $post_type_object = get_post_type_object($post_type);
+        if (!empty($post_types) && is_array($post_types)) {
+            foreach ($post_types as $post_type) {
+                $post_type_object = get_post_type_object($post_type);
 
-            // Get the Type name with ucfirst
-            $ucfirst = ucfirst($post_type_object->graphql_single_name);
+                // Get the Type name with ucfirst
+                $ucfirst = ucfirst($post_type_object->graphql_single_name);
 
-            // Register a new Edge Type
-            register_graphql_type('Previous' . $ucfirst . 'Edge', [
-                'fields' => [
-                    'node' => [
-                        'description' => __(
-                            'The node of the previous item',
-                            'wp-graphql'
-                        ),
-                        'type' => $ucfirst,
-                        'resolve' => function ($post_id, $args, $context) {
-                            return \WPGraphQL\Data\DataSource::resolve_post_object(
-                                $post_id,
-                                $context
-                            );
-                        }
+                // Register a new Edge Type
+                register_graphql_type('Previous' . $ucfirst . 'Edge', [
+                    'fields' => [
+                        'node' => [
+                            'description' => __(
+                                'The node of the previous item',
+                                'wp-graphql'
+                            ),
+                            'type' => $ucfirst,
+                            'resolve' => function ($post_id, $args, $context) {
+                                return \WPGraphQL\Data\DataSource::resolve_post_object(
+                                    $post_id,
+                                    $context
+                                );
+                            }
+                        ]
                     ]
-                ]
-            ]);
+                ]);
 
-            // Register the next{$type} field
-            register_graphql_field($ucfirst, 'previous' . $ucfirst, [
-                'type' => 'Previous' . $ucfirst . 'Edge',
-                'description' => __(
-                    'The previous post of the current post',
-                    'wp-graphql'
-                ),
-                'resolve' => function ($post_object, $args, $context) {
-                    global $post;
-                    $post = get_post($post_object->postId);
-                    if (is_post_type_hierarchical($post->post_type)) {
-	                    $post_id = get_previous_page_id($post);
-                    }
-                    else {
-	                    $prev_post = get_previous_post();
-						$post_id = $prev_post->ID;
+                // Register the next{$type} field
+                register_graphql_field($ucfirst, 'previous' . $ucfirst, [
+                    'type' => 'Previous' . $ucfirst . 'Edge',
+                    'description' => __(
+                        'The previous post of the current post',
+                        'wp-graphql'
+                    ),
+                    'resolve' => function ($post_object, $args, $context) {
+                        // global $post;
+                        $post = get_post($post_object->postId);
+                        if (is_post_type_hierarchical($post->post_type)) {
+    	                    $post_id = get_previous_page_id($post);
+                        }
+                        else {
+    	                    $prev_post = get_previous_post();
+    						$post_id = $prev_post->ID;
 
+                        }
+                        return $post_id;
                     }
-                    return $post_id;
-                }
-            ]);
+                ]);
+            }
         }
     }
-}
-add_action('graphql_register_types', 'gql_register_previous_post');
+    add_action('graphql_register_types', 'gql_register_previous_post');
 
 
-// Util function for previous page id
-function get_previous_page_id($page) {
-	return get_adjacent_page_id($page, -1);
-}
+/*
+ *  Util function for previous page id
+ */
+    function get_previous_page_id($page) {
+    	return get_adjacent_page_id($page, -1);
+    }
 
-// Util function for next page id
-function get_next_page_id($page) {
-	return get_adjacent_page_id($page, 1);
-}
+/*
+ * Util function for next page id
+ */
+    function get_next_page_id($page) {
+    	return get_adjacent_page_id($page, 1);
+    }
 
 
-// Util function for adjacent page id
-// returns adjacent page id
-// Params: $page -> page object from wordpress
-// Params: $direction -> Integer -1 or 1 indicating next or previous post 
-function get_adjacent_page_id($page, $direction) {
-	$args = array(
-				'post_type'         => $page->post_type,
-				'order'             => 'ASC',
-				'orderby'           => 'menu_order',
-				'post_parent'       => $page->post_parent,
-				'fields'            => 'ids',
-				'posts_per_page'    => -1
-			);
-	$pages = get_posts($args);
-	$current_key = array_search($page->ID, $pages);
-	$output = 0;
-	if( isset($pages[$current_key+$direction]) ) {
-		// Next page exists
-		$output = $pages[$current_key+$direction];
-	}
-	return $output;
-}
+/*
+ * Util function for adjacent page id
+ *
+ * Params: $page -> page object from wordpress
+ * Params: $direction -> Integer -1 or 1 indicating next or previous post
+ * returns adjacent page id
+ */
+    function get_adjacent_page_id($page, $direction) {
+    	$args = array(
+    		'post_type'         => $page->post_type,
+    		'order'             => 'ASC',
+    		'orderby'           => 'menu_order',
+    		'post_parent'       => $page->post_parent,
+    		'fields'            => 'ids',
+    		'posts_per_page'    => -1
+    	);
+
+    	$pages = get_posts($args);
+    	$current_key = array_search($page->ID, $pages);
+    	$output = 0;
+
+        if( isset($pages[$current_key+$direction]) ) {
+    		// Next page exists
+    		$output = $pages[$current_key+$direction];
+    	}
+    	return $output;
+    }
+
+
+/*
+ * Allow for more than 100 posts/pages to be returned.
+ * There is probably a better way to do what you are doing than changing this!
+ */
+    function allow_more_posts_per_query($amount, $source, $args, $context, $info) {
+        return 300;
+    }
+    //add_filter( 'graphql_connection_max_query_amount', 'allow_more_posts_per_query', 10, 5);
