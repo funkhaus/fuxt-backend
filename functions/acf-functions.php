3630b42cdf7c58e0d469db2868a696e3ac897b7c
Drew Baker <drewrb@gmail.com>
2020-02-22 19:12:56 -0800

    Fixed ACF site options setup

--- functions/acf-functions.php
+++ functions/acf-functions.php
@@ -2,24 +2,26 @@
 /*
  * Allow ACF to save a copy of it's settings to a JSON file
  */
-    function custom_acf_save_directory($path)
-    {
-        return get_stylesheet_directory() . '/acf';
-    }
-    add_filter('acf/settings/save_json', 'custom_acf_save_directory');
-
+	function custom_acf_save_directory($path) {
+	    return get_stylesheet_directory() . '/acf';
+	}
+	add_filter('acf/settings/save_json', 'custom_acf_save_directory');
 
 /*
  * Add ACF site options page
  */
-    if( function_exists('acf_add_options_page') ) {
-        // Add `Theme General Settings` sub page
-    	acf_add_options_page(array(
-    		'page_title' 	=> 'Site Options',
-    		'menu_title'	=> 'Site Options',
-    		'menu_slug' 	=> 'site-options',
-    		'capability'	=> 'edit_posts',
-    		'redirect'		=> false,
-        	'show_in_graphql' => true,
-    	));
-    }
+	function stackhaus_add_acf_options() {
+	    if( function_exists('acf_add_options_page') ) {
+	        // Add `Theme General Settings` sub page
+	    	acf_add_options_page(array(
+	    		'page_title' 	=> 'Site Options',
+	    		'menu_title'	=> 'Site Options',
+	    		'menu_slug' 	=> 'site-options',
+	    		'capability'	=> 'edit_posts',
+	    		'redirect'		=> false,
+	        	'show_in_graphql' => true,
+	        	'position'		  => '60.1'
+	    	));
+	    }
+	}
+	add_action('acf/init', 'stackhaus_add_acf_options');    
