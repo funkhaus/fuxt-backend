30f639a0b3e1e7ff99e58e401eae76b0d556eb3f
Drew Baker <drewrb@gmail.com>
2020-03-12 15:13:59 -0700

    Update theme-config.php

--- functions/theme-config.php
+++ functions/theme-config.php
@@ -7,12 +7,12 @@
 		set_post_thumbnail_size( 960, 540, false );
 		add_image_size( 'social-preview', 600, 315, true ); // Square thumbnail used by sharethis and facebook
 
-        // You may want to change these, but these defaults cover most use cases
-        add_image_size( 'small-preview', 375, 0, false );
-        add_image_size( 'medium-preview', 960, 0, false );
-        add_image_size( 'large-preview', 1280, 0, false );
+		// You may want to change these, but these defaults cover most use cases
+		add_image_size( 'small-preview', 375, 0, false );
+		add_image_size( 'medium-preview', 960, 0, false );
+		add_image_size( 'large-preview', 1280, 0, false );
 		add_image_size( 'fullscreen-small', 1920, 0, false );
-        add_image_size( 'fullscreen', 2560, 0, false );
+		add_image_size( 'fullscreen', 2560, 0, false );
 	}
 	add_action( 'after_setup_theme', 'custom_image_sizes' );
 
