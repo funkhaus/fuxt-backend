39c7b7ed775c347662c49fee9450edecdf8b5ee1
drewbaker <drewrb@gmail.com>
2020-11-18 15:53:17 +0100

    Update plugin-importer.php

--- functions/plugin-importer.php
+++ functions/plugin-importer.php
@@ -8,7 +8,7 @@
 
         // Change these values to install new versions of plugins
         $config = array(
-            'id'           => 'fuxt',             // Unique ID for hashing notices for multiple instances of TGMPA.
+            'id'           => 'fuxt',                  // Unique ID for hashing notices for multiple instances of TGMPA.
             'default_path' => '',                      // Default absolute path to bundled plugins.
             'menu'         => 'tgmpa-install-plugins', // Menu slug.
             'parent_slug'  => 'themes.php',            // Parent menu slug.
@@ -16,7 +16,7 @@
             'has_notices'  => true,                    // Show admin notices or not.
             'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
             'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-            'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+            'is_automatic' => true,                    // Automatically activate plugins after installation or not.
             'message'      => '',                      // Message to output right before the plugins table.
         );
 
