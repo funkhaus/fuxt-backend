0a848428da00cb45005818ece1c42aaefbe67480
Drew Baker <drewrb@gmail.com>
2020-11-19 14:30:13 +0100

    Removed menu location setup now that we can use menu name GQL

--- functions/theme-config.php
+++ functions/theme-config.php
@@ -24,14 +24,3 @@
 		update_option( 'home', 'https://fuxt.funkhaus.us' );
 	}
 	add_action('init', 'define_site_frontend_url');
-
-/*
- * Setup menu locations
- */
-	function register_custom_nav_menus() {
-		$menus = array(
-			'main-menu' => 'Main Menu'
-		);
-		register_nav_menus($menus);
-	}
-	add_action('after_setup_theme', 'register_custom_nav_menus');
