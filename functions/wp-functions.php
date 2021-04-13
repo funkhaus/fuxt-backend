400f3581fa050ea92ae2b038ff26acec40f35a37
rsm0128 <rsm0128@hotmail.com>
2021-04-14 06:54:02 +0900

    fixed flywheel rest api issue

--- functions/wp-functions.php
+++ functions/wp-functions.php
@@ -277,9 +277,14 @@ if ( defined('FLYWHEEL_CONFIG_DIR') ) {
     /*
      * Return the fuxt_home_url value when code requests the Site Address (URL)
      */
-    function fuxt_get_home_url( $url ) {
-        $fuxt_home_url = get_option( 'fuxt_home_url ' );
-        return $fuxt_home_url ?: $url;
+    function fuxt_get_home_url( $url, $path, $orig_theme ) {
+        if ( 'rest' !== $orig_theme ) {
+            $fuxt_home_url = get_option( 'fuxt_home_url ' );
+            if ( ! empty( $fuxt_home_url ) ) {
+                $url = $fuxt_home_url;
+            }
+        }
+        return $url;
     }
-    add_filter('option_home', 'fuxt_get_home_url');
+    add_filter('home_url', 'fuxt_get_home_url', 10, 3);
 }
