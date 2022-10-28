d1922cccb9ea8893cf0ca2be0730135ffd4af814
rsm0128 <rsm0128@hotmail.com>
2022-10-28 13:18:14 +0800

    phpcs fix for gutenberg-functions

--- functions/gutenberg-functions.php
+++ functions/gutenberg-functions.php
@@ -1,16 +1,26 @@
 <?php
 /**
- * Whitelist blocks for the Gutenberg editor
+ * Extends WP-Gutenberg-GraphQL function.
+ *
+ * @package fuxt-backend
+ */
+
+/**
+ * Whitelist blocks for the Gutenberg editor.
  * SEE https://github.com/WordPress/gutenberg/tree/trunk/packages/block-library/src
  *
+ * @param array $allowed_block_types Array of block type slugs.
  * @return array
  */
-function fuxt_block_whitelist($allowed_block_types) {
+function fuxt_block_whitelist( $allowed_block_types ) {
 
 	// Assuming you want all ACF custom blocks.
-	$acf_blocks = array_filter(get_dynamic_block_names(), function($name){
-		return strpos($name, 'acf/') !== false;
-	});
+	$acf_blocks = array_filter(
+		get_dynamic_block_names(),
+		function( $name ) {
+			return strpos( $name, 'acf/' ) !== false;
+		}
+	);
 
 	// Add specific core blocks to whitelist
 	$whitelist = array(
@@ -26,12 +36,12 @@ function fuxt_block_whitelist($allowed_block_types) {
 		'core/spacer',
 		'core/cover',
 		'core/html',
-        'core/video',
+		'core/video',
 	);
 
-	return array_merge($acf_blocks, $whitelist);
+	return array_merge( $acf_blocks, $whitelist );
 }
-add_filter('allowed_block_types_all', 'fuxt_block_whitelist');
+add_filter( 'allowed_block_types_all', 'fuxt_block_whitelist' );
 
 /**
  * Disable the fullscreen editor as default
