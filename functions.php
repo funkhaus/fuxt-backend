adf8331fa0d8eaa8d48d376891331d0092123483
rsm0128 <rsm0128@hotmail.com>
2022-10-28 13:45:42 +0800

    remove register-blocks.php as it's not used

--- functions.php
+++ functions.php
@@ -15,7 +15,6 @@ require_once get_template_directory() . '/functions/gql-functions.php';
 require_once get_template_directory() . '/functions/theme-config.php';
 require_once get_template_directory() . '/functions/gutenberg-functions.php';
 require_once get_template_directory() . '/functions/acf-functions.php';
-require_once get_template_directory() . '/blocks/register-blocks.php';
 require_once get_template_directory() . '/functions/cookie-manager.php';
 require_once get_template_directory() . '/functions/plugin-manifest.php';
 require_once get_template_directory() . '/functions/developer-role.php';
