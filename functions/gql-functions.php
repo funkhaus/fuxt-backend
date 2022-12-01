5f8e5bd2f4184ead2b9d1c1012fc30175bd8303c
rsm0128 <rsm0128@hotmail.com>
2022-12-01 17:58:35 +0800

    added in_same_parent option to nextPost and prevPost

--- functions/gql-functions.php
+++ functions/gql-functions.php
@@ -286,47 +286,13 @@ function gql_register_next_post() {
 							'type'        => 'String',
 							'description' => __( 'Comma-separated list of excluded term slugs.', 'fuxt' ),
 						),
+						'inSameParent'    => array(
+							'type'        => 'Boolean',
+							'description' => __( 'Whether post should be under the same parent. Default value: true for hierarchical, false for non-hierarchical', 'fuxt' ),
+						),
 					),
 					'resolve'     => function ( $post, $args, $context ) {
-						$post = get_post( $post->ID );
-
-						$in_same_term        = isset( $args['inSameTerm'] ) ? $args['inSameTerm'] : false;
-						$excluded_terms      = isset( $args['termNotIn'] ) ? array_map( 'intval', explode( ',', $args['termNotIn'] ) ) : array();
-						$taxonomy            = isset( $args['taxonomy'] ) ? $args['taxonomy'] : 'category';
-						$excluded_term_slugs = isset( $args['termSlugNotIn'] ) ? explode( ',', $args['termSlugNotIn'] ) : array();
-
-						// merge termNotIn and termSlugNotIn.
-						if ( ! empty( $excluded_term_slugs ) ) {
-							$term_ids = array_map(
-								function( $slug ) use ( $taxonomy ) {
-									$term = get_term_by( 'slug', $slug, $taxonomy );
-									if ( $term ) {
-										return $term->term_id;
-									}
-									return false;
-								},
-								$excluded_term_slugs
-							);
-
-							$excluded_terms = array_merge( $excluded_terms, array_filter( $term_ids ) );
-						}
-
-						$next_post = get_next_post(
-							$in_same_term,
-							$excluded_terms,
-							$taxonomy
-						);
-
-						if ( ! empty( $next_post ) ) {
-							return $next_post->ID;
-						}
-
-						$first_post = get_boundary_post( $in_same_term, $excluded_terms, true, $taxonomy );
-						if ( ! empty( $first_post ) ) {
-							return $first_post[0]->ID;
-						}
-
-						return null;
+						return fuxt_get_adjacent_post( $post->ID, $args, false );
 					},
 				)
 			);
@@ -400,46 +366,7 @@ function gql_register_previous_post() {
 						),
 					),
 					'resolve'     => function ( $post, $args, $context ) {
-						$post = get_post( $post->ID );
-
-						// Prepare arguments
-						$in_same_term        = isset( $args['inSameTerm'] ) ? $args['inSameTerm'] : false;
-						$excluded_terms      = isset( $args['termNotIn'] ) ? array_map( 'intval', explode( ',', $args['termNotIn'] ) ) : array();
-						$taxonomy            = isset( $args['taxonomy'] ) ? $args['taxonomy'] : 'category';
-						$excluded_term_slugs = isset( $args['termSlugNotIn'] ) ? explode( ',', $args['termSlugNotIn'] ) : array();
-
-						// merge termNotIn and termSlugNotIn.
-						if ( ! empty( $excluded_term_slugs ) ) {
-							$term_ids = array_map(
-								function( $slug ) use ( $taxonomy ) {
-									$term = get_term_by( 'slug', $slug, $taxonomy );
-									if ( $term ) {
-										return $term->term_id;
-									}
-									return false;
-								},
-								$excluded_term_slugs
-							);
-
-							$excluded_terms = array_merge( $excluded_terms, array_filter( $term_ids ) );
-						}
-
-						$prev_post = get_previous_post(
-							$in_same_term,
-							$excluded_terms,
-							$taxonomy
-						);
-
-						if ( ! empty( $prev_post ) ) {
-							return $prev_post->ID;
-						}
-
-						$last_post = get_boundary_post( $in_same_term, $excluded_terms, false, $taxonomy );
-						if ( ! empty( $last_post ) ) {
-							return $last_post[0]->ID;
-						}
-
-						return null;
+						return fuxt_get_adjacent_post( $post->ID, $args, true );
 					},
 				)
 			);
@@ -447,6 +374,175 @@ function gql_register_previous_post() {
 	}
 }
 add_action( 'graphql_register_types', 'gql_register_previous_post' );
+
+/**
+ * Get adjacent post.
+ *
+ * @param int     $post_id  Post ID.
+ * @param array   $args     Arguments to determine adjacent post.
+ * @param bool    $previous Optional. Whether to retrieve previous post. Default true.
+ * @return int|null Adjacent Post ID.
+ */
+function fuxt_get_adjacent_post( $post_id, $args, $previous = true ) {
+	$post = get_post( $post_id );
+
+	// Prepare arguments
+	$in_same_term        = isset( $args['inSameTerm'] ) ? $args['inSameTerm'] : false;
+	$excluded_terms      = isset( $args['termNotIn'] ) ? array_map( 'intval', explode( ',', $args['termNotIn'] ) ) : array();
+	$taxonomy            = isset( $args['taxonomy'] ) ? $args['taxonomy'] : 'category';
+	$excluded_term_slugs = isset( $args['termSlugNotIn'] ) ? explode( ',', $args['termSlugNotIn'] ) : array();
+	$in_same_parent      = isset( $args['inSameParent'] ) ? $args['inSameParent'] : is_post_type_hierarchical( $post->post_type );
+
+	// merge termNotIn and termSlugNotIn.
+	if ( ! empty( $excluded_term_slugs ) ) {
+		$term_ids = array_map(
+			function( $slug ) use ( $taxonomy ) {
+				$term = get_term_by( 'slug', $slug, $taxonomy );
+				if ( $term ) {
+					return $term->term_id;
+				}
+				return false;
+			},
+			$excluded_term_slugs
+		);
+
+		$excluded_terms = array_merge( $excluded_terms, array_filter( $term_ids ) );
+	}
+
+	$adjacent = $previous ? 'previous' : 'next';
+
+	if ( $in_same_parent ) {
+		// Add same parent filter.
+		add_filter( "get_{$adjacent}_post_where", 'fuxt_filter_same_parent_post', 10, 5 );
+	}
+	$adj_post = get_adjacent_post( $in_same_term, $excluded_terms, $previous, $taxonomy );
+
+	if ( $in_same_parent ) {
+		// Remove same parent filter.
+		remove_filter( "get_{$adjacent}_post_where", 'fuxt_filter_same_parent_post', 10 );
+	}
+
+	if ( ! empty( $adj_post ) ) {
+		return $adj_post->ID;
+	}
+
+	// Get last or fist post if it's prev on start or next on end.
+	// $last_post = fuxt_get_boundary_post( $in_same_term, $excluded_terms, ! $previous, $taxonomy, $in_same_parent );
+	if ( $in_same_parent ) {
+		add_action( 'pre_get_posts', 'fuxt_pre_get_posts_same_parent' );
+	}
+	$last_post = get_boundary_post( $in_same_term, $excluded_terms, ! $previous, $taxonomy );
+	if ( $in_same_parent ) {
+		remove_action( 'pre_get_posts', 'fuxt_pre_get_posts_same_parent' );
+	}
+
+	if ( ! empty( $last_post ) ) {
+		return $last_post[0]->ID;
+	}
+
+	return null;
+}
+
+/**
+ * Adds post_parent comparison statement to where clause.
+ *
+ * @param string       $where          The `WHERE` clause in the SQL.
+ * @param bool         $in_same_term   Whether post should be in a same taxonomy term.
+ * @param int[]|string $excluded_terms Array of excluded term IDs. Empty string if none were provided.
+ * @param string       $taxonomy       Taxonomy. Used to identify the term used when `$in_same_term` is true.
+ * @param WP_Post      $post           WP_Post object.
+ * @return string.
+ */
+function fuxt_filter_same_parent_post( $where, $in_same_term, $excluded_terms, $taxonomy, $post ) {
+	return $where .= sprintf( ' AND p.post_parent = %d', $post->post_parent );
+}
+
+/**
+ * Alter query vars to get the post in same parent and post_type.
+ *
+ * @param WP_Query $query Query object.
+ */
+function fuxt_pre_get_posts_same_parent( $query ) {
+	global $post;
+	$query->query_vars['orderby']     = 'menu_order';
+	$query->query_vars['post_parent'] = $post->post_parent;
+	$query->query_vars['post_type']   = $post->post_type;
+}
+
+
+/**
+ * Retrieves the boundary post.
+ *
+ * Boundary being either the first or last post by publish date within the constraints specified.
+ * by $in_same_term or $excluded_terms.
+ *
+ * @since 2.8.0
+ *
+ * @param bool         $in_same_term   Optional. Whether returned post should be in a same taxonomy term.
+ *                                     Default false.
+ * @param int[]|string $excluded_terms Optional. Array or comma-separated list of excluded term IDs.
+ *                                     Default empty.
+ * @param bool         $start          Optional. Whether to retrieve first or last post. Default true.
+ * @param string       $taxonomy       Optional. Taxonomy, if $in_same_term is true. Default 'category'.
+ * @param bool         $in_same_parent Optional. Whether returned post should be in the same parent.
+ * @return null|array Array containing the boundary post object if successful, null otherwise.
+ */
+function fuxt_get_boundary_post( $in_same_term = false, $excluded_terms = '', $start = true, $taxonomy = 'category', $in_same_parent = true ) {
+	$post = get_post();
+
+	if ( ! $post || ! is_single() || is_attachment() || ! taxonomy_exists( $taxonomy ) ) {
+		return null;
+	}
+
+	$query_args = array(
+		'posts_per_page'         => 1,
+		'order'                  => $start ? 'ASC' : 'DESC',
+		'update_post_term_cache' => false,
+		'update_post_meta_cache' => false,
+		'post_type'              => $post->post_type,
+	);
+
+	if ( $in_same_parent ) {
+		$query_args['post_parent'] = $post->post_parent;
+		$query_args['orderby']     = 'menu_order';
+	}
+
+	$term_array = array();
+
+	if ( ! is_array( $excluded_terms ) ) {
+		if ( ! empty( $excluded_terms ) ) {
+			$excluded_terms = explode( ',', $excluded_terms );
+		} else {
+			$excluded_terms = array();
+		}
+	}
+
+	if ( $in_same_term || ! empty( $excluded_terms ) ) {
+		if ( $in_same_term ) {
+			$term_array = wp_get_object_terms( $post->ID, $taxonomy, array( 'fields' => 'ids' ) );
+		}
+
+		if ( ! empty( $excluded_terms ) ) {
+			$excluded_terms = array_map( 'intval', $excluded_terms );
+			$excluded_terms = array_diff( $excluded_terms, $term_array );
+
+			$inverse_terms = array();
+			foreach ( $excluded_terms as $excluded_term ) {
+				$inverse_terms[] = $excluded_term * -1;
+			}
+			$excluded_terms = $inverse_terms;
+		}
+
+		$query_args['tax_query'] = array(
+			array(
+				'taxonomy' => $taxonomy,
+				'terms'    => array_merge( $term_array, $excluded_terms ),
+			),
+		);
+	}
+
+	return get_posts( $query_args );
+}
 
 /**
  * Set the default ordering of quieres in WP-GQL
